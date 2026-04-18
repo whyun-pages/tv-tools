8075f4276c3df3084d594228a0249bf5b7a734e8
yunnysunny <yunnysunny@gmail.com>
2026-04-18 15:01:37 +0800

    feat: fix missing ts defines

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export { decodeTvboxJson, parseTvboxJson } from './tvbox-json-decrypt.js';
-export type * from './types.js';
+export type {
+  Doh,
+  Live,
+  Parse,
+  Recordable,
+  Rule,
+  Site,
+  TVBoxConfig,
+} from './types.js';
